3771919ba79ae4e6d53eb433fea1cf3539e67e50
eth-p <eth-p+git@hidden.email>
2021-12-12 18:36:02 -0800

    Add button for "Another" quote

--- cmd/discord-inspirobot/cmd_inspire.go
+++ cmd/discord-inspirobot/cmd_inspire.go
@@ -25,44 +25,20 @@ func init() {
 				Description: "Get an inspirational quote from InspiroBot.",
 			},
 			Handler: func(ctx *bot.CommandContext, interaction bot.Interaction) {
-				url, err := generateInspirobotImage()
-
-				// If an inspirational quote could not be generated, tell the user and fail.
+				err := respondWithQuote(interaction)
 				if err != nil {
-					interaction.RespondPrivately(&discordgo.InteractionResponseData{
-						Embeds: []*discordgo.MessageEmbed{
-							{
-								Title:       "Unable to Inspire",
-								Description: err.Error(),
-								Color:       0xff0000,
-							},
-						},
-					})
-
 					ctx.Fatal(err)
 				}
-
-				// Get the title quote.
-				titleId, titleQuote := generateInspirationalQuote()
-
-				// Share the inspirational quote.
-				interaction.RespondPrivately(&discordgo.InteractionResponseData{
-					Embeds: []*discordgo.MessageEmbed{makeQuoteEmbed(titleQuote, url.String())},
-					Components: []discordgo.MessageComponent{
-						discordgo.ActionsRow{
-							Components: []discordgo.MessageComponent{
-								&discordgo.Button{
-									Label:    "Send to Channel",
-									Style:    discordgo.PrimaryButton,
-									CustomID: fmt.Sprintf("share:%v,%s", titleId, url.String()),
-								},
-							},
-						},
-					},
-				})
 			},
 
 			ComponentHandlers: map[string]bot.ComponentHandler{
+				"another": func(ctx *bot.ComponentContext, interaction bot.Interaction) {
+					err := respondWithQuote(interaction)
+					if err != nil {
+						ctx.Fatal(err)
+					}
+				},
+
 				"share": func(ctx *bot.ComponentContext, interaction bot.Interaction) {
 					// Parse the CustomData for the LRU cache key and the URL.
 					split := strings.SplitN(ctx.CustomData, ",", 2)
@@ -93,12 +69,63 @@ func init() {
 	})
 }
 
-func makeQuoteEmbed(titleQuote, url string) *discordgo.MessageEmbed {
+// respondWithQuote responds to an interaction with an InspiroBot quote.
+func respondWithQuote(interaction bot.Interaction) error {
+	// If an inspirational quote could not be generated, tell the user and fail.
+	url, err := generateInspirobotImage()
+	if err != nil {
+		interaction.RespondPrivately(&discordgo.InteractionResponseData{
+			Embeds: []*discordgo.MessageEmbed{
+				{
+					Title:       "Unable to Inspire",
+					Description: err.Error(),
+					Color:       0xff0000,
+				},
+			},
+		})
+
+		return err
+	}
+
+	// Get the title quote.
+	titleId, titleQuote := generateInspirationalQuote()
+
+	// Share the inspirational quote.
+	interaction.RespondPrivately(&discordgo.InteractionResponseData{
+		Embeds: []*discordgo.MessageEmbed{makeQuoteEmbed(titleQuote, url.String())},
+		Components: []discordgo.MessageComponent{
+			discordgo.ActionsRow{
+				Components: []discordgo.MessageComponent{
+					&discordgo.Button{
+						Label:    "Send to Channel",
+						Style:    discordgo.PrimaryButton,
+						CustomID: fmt.Sprintf("share:%v,%s", titleId, url.String()),
+					},
+					&discordgo.Button{
+						Label:    "Another",
+						Style:    discordgo.SecondaryButton,
+						CustomID: "another",
+					},
+				},
+			},
+		},
+	})
+
+	return nil
+}
+
+// makeQuoteEmbed creates a discordgo.MessageEmbed instance for an InspiroBot quote.
+func makeQuoteEmbed(title, url string) *discordgo.MessageEmbed {
 	return &discordgo.MessageEmbed{
-		Title: titleQuote,
+		Title: title,
 		Color: 0x2cc406,
 		Image: &discordgo.MessageEmbedImage{
 			URL: url,
+		},
+		Author: &discordgo.MessageEmbedAuthor{
+			URL:     "https://inspirobot.me/",
+			Name:    "InspiroBot",
+			IconURL: "https://inspirobot.me/website/images/inspirobot-dark-green.png",
 		},
 	}
 }
--- go.mod
+++ go.mod
@@ -3,15 +3,18 @@ module github.com/eth-p/discord-inspirobot
 go 1.17
 
 require (
-	github.com/bwmarrin/discordgo v0.23.3-0.20211204170245-092735083ddf // indirect
+	github.com/bwmarrin/discordgo v0.23.3-0.20211204170245-092735083ddf
+	github.com/hashicorp/golang-lru v0.5.4
+	github.com/urfave/cli/v2 v2.3.0
+	k8s.io/klog/v2 v2.30.0
+)
+
+require (
 	github.com/cpuguy83/go-md2man/v2 v2.0.0-20190314233015-f79a8a8ca69d // indirect
 	github.com/go-logr/logr v1.2.0 // indirect
 	github.com/gorilla/websocket v1.4.2 // indirect
-	github.com/hashicorp/golang-lru v0.5.4 // indirect
 	github.com/russross/blackfriday/v2 v2.0.1 // indirect
 	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
-	github.com/urfave/cli/v2 v2.3.0 // indirect
 	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b // indirect
 	golang.org/x/sys v0.0.0-20201119102817-f84b799fce68 // indirect
-	k8s.io/klog/v2 v2.30.0 // indirect
 )
